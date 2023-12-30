4fddd961b5b9bddfe53e96607026e91487f66dbd
Mr. Soul Hacker <108230766+ALFAJ01@users.noreply.github.com>
2023-12-30 12:35:29 +0530

    Update soulhacker.js

--- proposal/soulhacker.js
+++ proposal/soulhacker.js
@@ -1,45 +1,37 @@
 var i = 0;
 var txt1 =
- "Hi Love.....! <<        Now I want to say something special to you. <<<        So , Please read everything carefully...!                                      > When I saw you for the first time In A Ur Brother Status< You seemed something Special to me. <<         As the days go < you get closer to me from my side ....! <<              I don't know the reason why your thoughts always resonate inside my mind...!                           > Everything about you is always interesting for me But Honestly Something You need to Change too...!           << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in the future...!                           > I Love my Parents so much than anything else in this world....!          << Now You are the only person whom I love equally with my parents....!                               >I Love U <SweetHeart Mona 💕 .....!         << Kept It Secret🔐.. by @محمد أفسارول الحق >  |  ";
+  "Hi Love.....!  <<               Now I want to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time In A Ur Brother Status< You seemed something Special to me.  <<                  As the days go < you get closer to me from my side ....! <<                           I don't know the reason why your thoughts always resonate inside my mind...!                                                     > Everything about you is always interesting for me But Honestly Something You need to Change too...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in the future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the only person  whom I love equally with my parents....!                                                             >I Love U <SweetHeart Mona 💕 .....!                  << Kept It Secret🔐.. by @محمد أفسارول الحق  >    |   ";
 var speed = 55;
+
 typeWriter();
 
-function
- 
-typeWriter() {
+function typeWriter() {
   if (i < txt1.length) {
-    if (txt1.charAt(i) == "<") {
+    if (txt1.charAt(i) == "<")
       document.getElementById("text1").innerHTML += "</br>";
-    } else
- 
-if (txt1.charAt(i) == ">") {
+    else if (txt1.charAt(i) == ">")
       document.getElementById("text1").innerHTML = "";
-    } else
- 
-if (txt1.charAt(i) == "|") {
+    else if (txt1.charAt(i) == "|") {
       $(".bg_heart").css("background-image", "");
-      i++; // Increment i before calling askForAcceptance
-      askForAcceptance(); // Call directly, no setTimeou
-    } else {
-      document.getElementById("text1").innerHTML += txt1.charAt(i);
-      i++;
-    }
-
-    // Delay for the next character
+      setTimeout(askForAcceptance, 500); // Adjust the delay as needed
+    } else document.getElementById("text1").innerHTML += txt1.charAt(i);
+    i++;
     setTimeout(typeWriter, speed);
   }
 }
 
 function askForAcceptance() {
- var userResponse = prompt("Do you accept my proposal? (Yes/No)");
- if (userResponse && userResponse.toLowerCase() === "yes") {
-  alert("Thank you for accepting! Your response has been noted.");
-  openGoogleForm();
- } else {
-  alert("Oh no! If you change your mind, let me know. Your response has been noted.");
- }
+  var userResponse = prompt("Do you accept my proposal? (Yes/No)");
+
+  if (userResponse && userResponse.toLowerCase() === "yes") {
+    alert("Thank you for accepting! Your response has been noted.");
+    openGoogleForm();
+  } else {
+    alert("Oh no! If you change your mind, let me know. Your response has been noted.");
+  }
 }
+
 function openGoogleForm() {
- var googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSfMsBgcH5r67Mg5hcTL4qsEy-nTW0fBpoQQBMkbnDIPdpc-VQ/viewform?usp=sf_link"; // Replace with your Google Form link
- window.open(googleFormLink, "_blank");
+  var googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLSfMsBgcH5r67Mg5hcTL4qsEy-nTW0fBpoQQBMkbnDIPdpc-VQ/viewform?usp=sf_link"; // Replace with your Google Form link
+  window.open(googleFormLink, "_blank");
 }
