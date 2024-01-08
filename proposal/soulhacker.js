00e08a244ae2c747150fa48b4609273958f13026
Mr. Soul Hacker <108230766+ALFAJ01@users.noreply.github.com>
2024-01-08 10:52:50 +0530

    Update soulhacker.js

--- proposal/soulhacker.js
+++ proposal/soulhacker.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt1 =
   "Hi My Self ..! << Now I want to say something special to you. <<< So , Please read everything carefully...! > When I saw you for the first time  < You seemed something Special to me. << As the days go < you get closer to me from my side ....! << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting for me But Honestly Something You need to Change too...! << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in the future...! > I Love my Parents so much than anything else in this world....! << Now You are the only person whom I love equally with my parents....! >I💔 <<<! << By @محمد أفسارول الحق > |          . ";
-var speed = 45;
+var speed = 85;
 
 typeWriter();
 
