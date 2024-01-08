c230139aa5e61a7f631476966e22341d5a9da8e9
Mr. Soul Hacker <108230766+ALFAJ01@users.noreply.github.com>
2024-01-08 11:09:49 +0530

    Update soulhacker.js

--- proposal/soulhacker.js
+++ proposal/soulhacker.js
@@ -1,19 +1,17 @@
 var i = 0;
 var txt1 =
-  "Hi My Self ..! << Now I want to say something special to you. <<< So , Please read everything carefully...! > When I saw you for the first time  < You seemed something Special to me. << As the days go < you get closer to me from my side ....! << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting for me But Honestly Something You need to Change too...! << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in the future...! > I Love my Parents so much than anything else in this world....! << Now You are the only person whom I love equally with my parents....! >I💔 <<<! << By @محمد أفسارول الحق > |          . ";
-var speed = 85;
+  "Hi My Self ..! << Now I want to say something special to you. <<< So , Please read everything carefully...! > When I saw you for the first time < You seemed something Special to me. << As the days go < you get closer to me from my side ....! << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting for me But Honestly Something You need to Change too...! << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in the future...! > I Love my Parents so much than anything else in this world....! << Now You are the only person whom I love equally with my parents....! >I💔 <<<! << By @محمد أفسارول الحق > |     . ";
+var speed = 75; // Adjust the value for slower text speed
 
 typeWriter();
 
 function typeWriter() {
   if (i < txt1.length) {
-    if (txt1.charAt(i) == "<")
-      document.getElementById("text1").innerHTML += "</br>";
-    else if (txt1.charAt(i) == ">")
-      document.getElementById("text1").innerHTML = "";
+    if (txt1.charAt(i) == "<") document.getElementById("text1").innerHTML += "</br>";
+    else if (txt1.charAt(i) == ">") document.getElementById("text1").innerHTML = "";
     else if (txt1.charAt(i) == "|") {
       $(".bg_heart").css("background-image", "");
-      askForAcceptance()
+      askForAcceptance();
     } else document.getElementById("text1").innerHTML += txt1.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
