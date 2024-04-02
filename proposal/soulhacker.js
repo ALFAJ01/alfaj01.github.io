bfda4d7fd827400cb0659ec9f29d60dbacd2e6a0
Mr. Soul Hacker <108230766+ALFAJ01@users.noreply.github.com>
2024-04-02 07:59:33 +0530

    Update soulhacker.js

--- proposal/soulhacker.js
+++ proposal/soulhacker.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt1 =
-   "Assalamualaikum 🙂 ..! << Now I want to say something special to you. <<< So, Please read everything carefully...! > When I saw you for the first time infronts of zoology department while you walking with your friends. <<< You seemed something Special to me. And my heart wanted to talk to you > But I can't make a courage to talk you And when I try to make a courage  to talk you than my inner soul talk to me Brothes you are in fasting and wnated to do this later on i make courage but at that point you are Vanish think are stoped here. > If I say that infornt of zoology departmentis is not the 1st i met than  it  will't go wrong i saw you several time but i can't figure out who are really you are << But that day I finnaly  figured out who are you. << Later on I try to stop everything here but i can't that why i write this 🙃 <when I looked at you a moment of your glances from this moment to till now << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting for me. << I am somewhat nervous Because I haven't said these words to anyone and I won't say it anyone in future if you wish so. But the Honest things is what you know I say these words before you but Everyone response me go ahead achived your goal you will get something better but believe  me i never in a relationship till now with anyone . >And I Don't know why my Hert say to me that the better one is You👆🏻 .> I Love my Parents and my family so much than anything else in this 🌎. But  << Now my Hert the only person whom I love equally with my parents....! > <<Afsarul> |   . ";
+   "Assalamualaikum 🙂 ..! << Now I want to say something special to you. <<< So, Please read everything carefully...! > When I saw you for the first time infronts of zoology department while you walking with your friends. <<< You seemed something Special to me. And my heart wanted to talk to you > But I can't make a courage to talk you And when I try to make a courage  to talk you than my inner soul talk to me Brothes you are in fasting and wnated to do this later on i make courage but at that point you are Vanish think are stoped here. > If I say that infornt of zoology departmentis is not the 1st i met than  it  will't go wrong i saw you several time and if i say i still remember your registration from number 122 is not it  because 123 is mine ? but i can't figure out who are really you are << But that day I finnaly  figured out who are you. << Later on I try to stop everything here but i can't that why i write this 🙃 <when I looked at you a moment of your glances from this moment to till now << I don't know the reason why your thoughts always resonate inside my mind...! > Everything about you is always interesting for me. << I am somewhat nervous Because I haven't said these words to anyone and I won't say it anyone in future if you wish so. But the Honest things is what you know I say these words before you but Everyone response me go ahead achived your goal you will get something better but believe  me i never in a relationship till now with anyone . >And I Don't know why my Hert say to me that the better one is You👆🏻 .> I Love my Parents and my family so much than anything else in this 🌎. But  << Now my Hert the only person whom I love equally with my parents....! > <<Afsarul> |   . ";
 var speed = 100; // Adjusted speed for slower reading
 
 typeWriter();
@@ -43,7 +43,12 @@ function askForAcceptance() {
     openGoogleForm();
   } else {
     alert("Oh no! If you change your mind, let me know. Your response has been noted.");
+    aprilphool();
   }
+}
+function aprilphool() {
+  var web = "https://alfaj01.github.io/2024"; // Replace with your Google Form link
+  window.open(web, "_blank");
 }
 
 function openGoogleForm() {
